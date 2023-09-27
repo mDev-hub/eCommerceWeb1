20bab4ec502c5837167eb2c657d3a67388260877
M Developer Hub <123229020+mDev-hub@users.noreply.github.com>
2023-09-27 19:55:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to open the contact page or a specific link
+function openContactPage() {
+    // You can replace 'contact.html' with the actual link you want to open.
+    window.location.href = 'https://media.istockphoto.com/id/1473673099/photo/mockup-poster-frame-on-the-wall-of-living-room-luxurious-apartment-background-with.jpg?s=1024x1024&w=is&k=20&c=srteB0lWoHUU7x9Bpbu3BBHH9mwHM1iwXAIzTWFZYTo=';
+}
